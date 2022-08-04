66d8025d062e6d677bd1607e0d7de20c999aefa8
isra2021 <egancarry@gmail.com>
2022-08-04 03:50:58 -0300

    fix

--- todo/views.py
+++ todo/views.py
@@ -5,7 +5,7 @@ from rest_framework import status
 
 from .models import Task 
 from .serializers import TaskSerializer
-from core.models import User
+from django.contrib.auth import get_user_model
 
 
 class TaskModelViewSet(GenericViewSet):
