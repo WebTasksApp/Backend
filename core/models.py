29dc56e303f3b873dab647fbb5ba36be03394650
isra2021 <egancarry@gmail.com>
2022-08-04 02:30:47 -0300

    fix

--- core/models.py
+++ core/models.py
@@ -4,7 +4,7 @@ from todo.models import Task
 
 class User(AbstractUser):
     tasks = models.ManyToManyField(Task)
-    email = models.EmailField(unique=True)
+    #email = models.EmailField(unique=True)
     #USERNAME_FIELD = 'email'
     #REQUIRED_FIELDS = []
 
