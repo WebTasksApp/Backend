daf39f8d13ff2a19606414b6676ff9d2615536d9
isra2021 <egancarry@gmail.com>
2022-08-04 02:38:45 -0300

    fix

--- core/models.py
+++ core/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import AbstractUser
 from todo.models import Task
 
 class User(AbstractUser):
-    tasks = models.ManyToManyField(Task)
+    tasks = models.ManyToManyField(Task, null=True)
     #email = models.EmailField(unique=True)
     #USERNAME_FIELD = 'email'
     #REQUIRED_FIELDS = []
